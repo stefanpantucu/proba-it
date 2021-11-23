f7f947b2986803ae05b6c4d3145ef913a49db1e3
stefanpantucu <alexandrupantucu@gmail.com>
2021-11-23 13:01:56 +0200

    added get, get{id} and patch for /contact-requests

--- routes/contact-requests.js
+++ routes/contact-requests.js
@@ -3,11 +3,24 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const contactModel = require('../models/contact-requests');
 
+router.get('/', async (req, res) => {
+    var contactArray = await contactModel.find();
+
+    res.send(contactArray);
+})
+
+router.get('/:id', async (req, res) => {
+    console.log(req.params.id);
+
+    const contactRequest = await contactModel.findOne({_id: new mongoose.Types.ObjectId(req.params.id)});
+    return res.send(contactRequest);
+})
+
 router.post('/', async (req, res) => {
     const { name, email, message } = req.body;
 
-    if ((typeof name !== String) || (typeof message !== String) ||
-        (typeof email !== String)) {
+    if ((typeof name !== typeof '') || (typeof message !== typeof '') ||
+        (typeof email !== typeof '')) {
         console.log('Invalid data type');
         return res.status(400).send();
     }
@@ -26,6 +39,29 @@ router.post('/', async (req, res) => {
         console.log('Invalid request format!\n' + error);
         return res.status(400).send();
     }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const id = req.params.id; // get object id
+        const { is_resolved } = req.body;
+
+
+        if (typeof is_resolved != typeof true)
+            return res.status(400).send(); // is_resolved is not boolean
+
+        const result = await contactModel.findByIdAndUpdate(id, {is_resolved});
+
+        if (result == null)
+            return res.status(404).send();
+
+        return res.send(result);
+    }
+    catch(error) {
+        console.log(error.message);
+        res.status(404).send();
+    }
+
 })
 
 module.exports = router;
