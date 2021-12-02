e909ef7df3ff84cdea44e5b8e6bcbd2b5d099f8a
stefanpantucu <alexandrupantucu@gmail.com>
2021-12-02 17:38:47 +0200

    finished 4.Tutoring Class

--- routes/tutoringClasses.js
+++ routes/tutoringClasses.js
@@ -76,6 +76,10 @@ router.patch('/:id', reqAuth, async (req, res) => {
             console.log('Description field is required');
             return res.status(400).send();
         }
+        if (description.length > 500) {
+            console.log('Description should have max 500 characters');
+            return res.status(400).send();
+        }
 
         // update tutoring class
         await Tutoring.findByIdAndUpdate(id, { description });
@@ -83,13 +87,49 @@ router.patch('/:id', reqAuth, async (req, res) => {
         const teacher = await User.findOne({_id: new mongoose.Types.ObjectId(req.body.teacher_id)});
 
         let classesArray = teacher.tutoring_classes;
-        classesArray.forEach(tclass => {
-            if (JSON.stringify(tclass.id) === JSON.stringify(tclass._id))
-                tclass.description = description;
+        classesArray.forEach(elem => {
+            if (JSON.stringify(elem.id) === JSON.stringify(tclass._id))
+                elem.description = description;
         })
 
         // update tutoring class in teacher's list
         await User.findByIdAndUpdate(teacher._id, {tutoring_classes: classesArray});
+
+        return res.status(200).send();
+    }
+    catch(error) {
+        console.log(error.message);
+        return res.status(500).send();
+    }
+})
+
+router.delete('/:id', reqAuth, async (req, res) => {
+    try {
+        const id = req.params.id;
+        const tclass = await Tutoring.findOne({_id: new mongoose.Types.ObjectId(id)});
+
+        if (tclass == null) {
+            console.log('No tutoring class has this id:' + id);
+            return res.status(404).send();
+        }
+
+        const teacher_id = tclass.teacher_id;
+        const teacher = await User.findOne({_id: new mongoose.Types.ObjectId(teacher_id)});
+        let classesArray = teacher.tutoring_classes;
+        let index = -1;
+
+        classesArray.forEach(elem => {
+            if (JSON.stringify(elem.id) === JSON.stringify(tclass._id))
+                index = classesArray.indexOf(elem);
+        })
+
+        classesArray.splice(index, 1); // remove tutoring class from list
+
+        // update teacher's list
+        await User.findByIdAndUpdate(teacher_id, {tutoring_classes: classesArray});
+
+        // delete the tutoring class from the db
+        await Tutoring.findByIdAndDelete(id);
 
         return res.status(200).send();
     }
